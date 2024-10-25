270dd6ba46a324149f39bb138f0652f59df73c12
alfredo802 <a.herrerafigueroa@uandresbello.edu>
2024-10-24 22:46:35 -0600

    Add files via upload

---
+++ Data_Science_Tools and Ecosystem.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem
+
+# Este cuaderno es utilizado como resumen y muestra el ecosistema de la ciencia de datos  por medio de varios ejercicio, en donde exploraremos el lenguaje, aplicacion de bibliotecas, operaciones aritmeticas y otras herramientas que son fundamentales para para la ciencia de datos por medio de Python.
+# 
+
+# Ejercicio 3
+
+# Los lenguajes mas populares para ciencia de datos son:
+# 1 - R
+# 2-SQL
+# 3-Python
+# 4-Julia
+
+# Las libreria mas usadas en ciencia de datos son las siguientes:
+# 1. Pandas
+# 2. NumPy
+# 3. Matplotlib
+# 4. Scikit-learn
+
+# | Data Science Tools       |
+# |--------------------------|
+# | Jupyter Notebook         |
+# | RStudio                  |
+# | Apache Zeppelin          |
+# 
+
+# ### A continuacion se muestras algunas evaluaciones aritmeticas realizadas con Python.
+
+# Suma: 5 + 5 = 10
+# 
+# Resta: 8 - 4 = 4
+# 
+# Multiplicación = 4* 2 = 8
+# 
+# Division: 20 / 2 = 10
+# 
+# Exponentes: 2 ** 4 = 16
+# 
+# Parantesis  y mezcla de operaciones: (4 * 2) + 2 = 10
+# 
+# Mezcla de Operaciones con exonentes y división:
+# 2 ** 4 / 2 + 2 = 4.0
+# 
+# Operaciones con parentesis y orden de desarrollo:
+# (4 + (3 * 2)) - 4 = 6
+# 
+# Exponentiation and square roots:
+# 25 ** (1/2) = 5.0 (Raiz cuadrada de 25)
+# 
+# Operaciones multiples con parentesis y orden de desarrollo:
+# (1 * 2) + (2 ** 2) - (4 / 2) = 3.0
+
+# In[11]:
+
+
+# Codigo para convertir minutos en horas
+# Para convertir 600 minutos  se de dividir por 60 y resultado son hora:
+600 / 60
+
+
+# In[5]:
+
+
+4+2
+
+
+# Ejercicio 10 
+# **Objetivos:**
+# - Enumerar los lenguajes más populares para la ciencia de datos.
+# - Enumerar las bibliotecas más utilizadas para la ciencia de datos.
+# - Crear una tabla de herramientas de la ciencia de datos.
+# - Demostrar expresiones aritméticas simples en Python.
+
+# Ejercicio 11 
+# Autor Alfredo Herrera
+
+# In[ ]:
+
+
+
+
